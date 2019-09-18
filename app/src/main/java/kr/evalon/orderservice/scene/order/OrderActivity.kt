ffd1975be8fb94826dd0ad5ea7a516f2e2996d70
Heukhyeon <heukhyeon@gmail.com>
2019-09-18 15:24:32 +0900

    OrderActivity CategoryListLiveData Connect

--- app/src/main/java/kr/evalon/orderservice/scene/order/OrderActivity.kt
+++ app/src/main/java/kr/evalon/orderservice/scene/order/OrderActivity.kt
@@ -2,11 +2,31 @@ package kr.evalon.orderservice.scene.order
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import kr.evalon.orderservice.livedata.CategoryListLiveData
+import kr.evalon.orderservice.models.ItemCategory
 
 class OrderActivity : AppCompatActivity() {
+    private val categoryLiveData = CategoryListLiveData()
+    private val vm : OrderVm
+    get() = ViewModelProviders.of(this).get(OrderVm::class.java)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        categoryLiveData.observe(this, Observer(this::onChangeCategoryList))
+    }
+
+    private fun onChangeCategoryList(categories:List<ItemCategory>) {
+        categories.map { model ->
+            val vm = CategoryVm(model, vm.orderItemsLiveData)
+            vm.selected.observe(this, Observer {
+                if(it) onChangeCategorySelected(model)
+            })
+        }
+    }
+
+    private fun onChangeCategorySelected(category: ItemCategory){
 
     }
 }
--- app/src/main/java/kr/evalon/orderservice/scene/order/OrderVm.kt
+++ app/src/main/java/kr/evalon/orderservice/scene/order/OrderVm.kt
@@ -2,7 +2,10 @@ package kr.evalon.orderservice.scene.order
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import kr.evalon.orderservice.models.OrderItem
 
 class OrderVm(app:Application) : AndroidViewModel(app) {
+    val orderItemsLiveData = MutableLiveData<List<OrderItem>>()
     val categoryAdapter = CategoryAdapter()
 }
