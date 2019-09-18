e7d9fd8bcae8e2f15f57ad227e5919379ca1a4cf
Heukhyeon <heukhyeon@gmail.com>
2019-09-18 14:44:53 +0900

    BaseItem에 categoryCodes 추가

--- app/src/main/java/kr/evalon/orderservice/models/ItemModels.kt
+++ app/src/main/java/kr/evalon/orderservice/models/ItemModels.kt
@@ -3,21 +3,23 @@ package kr.evalon.orderservice.models
 abstract class BaseItem(
     val code:String,
     val name:String,
-    val price:Int
+    val price:Int,
+    val categoryCodes: List<String>
 )
 
 class MenuItem(
     code:String,
     name:String,
     price:Int,
+    categoryCodes:List<String>,
     val discountPrice:Int,
-    val thumbnailUrl:String,
-    val categoryCodes:List<String>
-): BaseItem(code, name, price)
+    val thumbnailUrl:String
+): BaseItem(code, name, price, categoryCodes)
 
 class OrderItem(
     code:String,
     name:String,
     price:Int,
+    categoryCodes:List<String>,
     val count:Int
-): BaseItem(code, name, price)
+): BaseItem(code, name, price,categoryCodes)
