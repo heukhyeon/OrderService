7eb5004c58b95f5ba34bd96fd4c39aad62133802
heukhyeon <heukhyeon@gmail.com>
2019-09-25 20:53:50 +0900

    select livedata match

--- app/src/main/java/kr/evalon/orderservice/scene/order/option/OptionSelectHeaderVm.kt
+++ app/src/main/java/kr/evalon/orderservice/scene/order/option/OptionSelectHeaderVm.kt
@@ -7,14 +7,9 @@ import kr.evalon.orderservice.models.MenuItem
 
 class OptionSelectHeaderVm(val model:ItemOption, targetItems:List<MenuItem>) {
     val expandedLiveData = MutableLiveData<Boolean>().apply { value = false }
-    val childVmList = targetItems.map { OptionSelectRowVm(it) }
-    val selectChangedLiveData = MediatorLiveData<OptionSelectRowVm>().apply {
-        childVmList.forEach {child->
-            addSource(child.selectActionLiveData){
-                value = child
-            }
-        }
-    }
+    val childVmList = targetItems.map { OptionSelectRowVm(it, selectChangedLiveData) }
+    private val selectChangedLiveData = MediatorLiveData<OptionSelectRowVm>()
+
     init {
         require(model.targetItems == targetItems.map { it.code }){
             "ItemOption is Not Matched! " + model.code
--- app/src/main/java/kr/evalon/orderservice/scene/order/option/OptionSelectRowVm.kt
+++ app/src/main/java/kr/evalon/orderservice/scene/order/option/OptionSelectRowVm.kt
@@ -2,19 +2,28 @@ package kr.evalon.orderservice.scene.order.option
 
 import android.graphics.Color
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import kr.evalon.orderservice.livedata.ActionLiveData
 import kr.evalon.orderservice.models.MenuItem
 
-class OptionSelectRowVm(val model:MenuItem) {
-    val selectedLiveData = MutableLiveData<Boolean>().apply { value = false }
+class OptionSelectRowVm(val model:MenuItem, selectGroupLiveData:MediatorLiveData<OptionSelectRowVm>) {
+    private val selectedLiveData: LiveData<Boolean> = Transformations.map(selectGroupLiveData){
+        it == this
+    }
     val selectedColorLiveData: LiveData<Int> = Transformations.map(selectedLiveData) {
         it ?: return@map Color.BLACK
         if(it) return@map Color.GREEN else return@map Color.BLACK
     }
+    private val selectActionLiveData = ActionLiveData()
 
-    val selectActionLiveData = ActionLiveData()
+    init {
+        selectGroupLiveData.addSource(selectActionLiveData){
+            it ?: return@addSource
+            selectGroupLiveData.value = this
+        }
+    }
 
     fun select() = selectActionLiveData.click()
 }
