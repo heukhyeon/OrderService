4343245569656ffe5146f271f44ea53eb1d1f880
heukhyeon <heukhyeon@gmail.com>
2019-09-25 20:14:54 +0900

    OptionSelectAdapter

--- app/src/main/java/kr/evalon/orderservice/scene/order/option/OptionRowViewHolder.kt
+++ app/src/main/java/kr/evalon/orderservice/scene/order/option/OptionRowViewHolder.kt
@@ -1,5 +1,23 @@
 package kr.evalon.orderservice.scene.order.option
 
-class OptionRowViewHolder {
+import android.view.ViewGroup
+import androidx.lifecycle.LifecycleOwner
+import kr.evalon.orderservice.BR
+import kr.evalon.orderservice.DataBindHolder
+import kr.evalon.orderservice.R
 
+class OptionRowViewHolder(parent: ViewGroup) :
+    DataBindHolder(parent, R.layout.item_menuitem_option_row) {
+
+
+    fun bind(owner: LifecycleOwner, vm: OptionSelectRowVm, expanded: Boolean) {
+        bind.lifecycleOwner = owner
+        bind.setVariable(BR.vm, vm)
+        val param = itemView.layoutParams ?: ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        param.height = if (expanded) ViewGroup.LayoutParams.WRAP_CONTENT else 0
+        itemView.layoutParams = param
+    }
 }
---
+++ app/src/main/java/kr/evalon/orderservice/scene/order/option/OptionSelectAdapter.kt
@@ -0,0 +1,80 @@
+package kr.evalon.orderservice.scene.order.option
+
+import android.transition.TransitionManager
+import android.view.ViewGroup
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.OnLifecycleEvent
+import androidx.recyclerview.widget.RecyclerView
+import kr.evalon.orderservice.DataBindHolder
+import java.lang.IllegalStateException
+
+class OptionSelectAdapter(var owner: LifecycleOwner?) : RecyclerView.Adapter<DataBindHolder>(),
+    LifecycleObserver {
+
+    init {
+        requireNotNull(owner).lifecycle.addObserver(this)
+    }
+
+    private val buffer = ArrayList<OptionSelectHeaderVm>()
+    private val headerIndexes = ArrayList<Int>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataBindHolder {
+        return if (viewType == 0) OptionHeaderViewHolder(parent)
+        else OptionRowViewHolder(parent)
+    }
+
+    override fun getItemCount(): Int {
+        return buffer.sumBy { it.childVmList.size + 1 }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        val match = headerIndexes.first { it >= position }
+        return if (match == position) 0
+        else 1
+    }
+
+    override fun onBindViewHolder(holder: DataBindHolder, position: Int) {
+        val matchIndex = headerIndexes.indexOfFirst { it >= position }
+        val header = buffer[matchIndex]
+        val owner = owner ?: return
+        when (holder) {
+            is OptionHeaderViewHolder -> holder.bind(owner, header)
+            is OptionRowViewHolder -> {
+                val item = header.childVmList[position - headerIndexes[matchIndex] - 1]
+                holder.bind(owner, item, header.expandedLiveData.value!!)
+            }
+            else -> throw IllegalStateException()
+        }
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    private fun onDestroyed() {
+        owner = null
+    }
+
+    fun replaceOptions(options: List<OptionSelectHeaderVm>) {
+        buffer.clear()
+        headerIndexes.clear()
+        var cnt = 0
+        options.forEach {
+            buffer.add(it)
+            headerIndexes.add(cnt)
+            cnt += it.childVmList.size + 1
+        }
+        notifyDataSetChanged()
+    }
+
+    fun expandChanged(vm: OptionSelectHeaderVm, parentView: RecyclerView) {
+        buffer.forEachIndexed { index, header ->
+            val before = header.expandedLiveData.value
+            val after = header == vm
+            if (before != after) {
+                header.expandedLiveData.value = after
+                notifyItemRangeChanged(headerIndexes[index], header.childVmList.size + 1)
+            }
+        }
+        TransitionManager.beginDelayedTransition(parentView)
+    }
+}
