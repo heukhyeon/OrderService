eb0a73b9febbcb7367c2a62b918176458b499231
Heukhyeon <heukhyeon@gmail.com>
2019-09-26 16:35:32 +0900

    OrderItem finalPrice exclude

--- app/src/main/java/kr/evalon/orderservice/models/OrderItem.kt
+++ app/src/main/java/kr/evalon/orderservice/models/OrderItem.kt
@@ -1,29 +1,33 @@
 package kr.evalon.orderservice.models
 
-open class OrderItem(
-    code:String = "",
-    name:String = "",
-    price:Int = 0,
-    categoryCodes:List<String> = emptyList(),
-    count:Int = 0,
-    val optionItems: List<OrderItem> = emptyList()
-): BaseItem(code, name, price,categoryCodes){
+import com.google.firebase.database.Exclude
 
-    constructor(item:BaseItem, options:List<OrderItem> = emptyList()):this(
+open class OrderItem(
+    code: String = "",
+    name: String = "",
+    price: Int = 0,
+    categoryCodes: List<String> = emptyList(),
+    count: Int = 0,
+    val optionItems: List<OrderItem> = emptyList()
+) : BaseItem(code, name, price, categoryCodes) {
+
+    constructor(item: BaseItem, options: List<OrderItem> = emptyList()) : this(
         item.code,
         item.name,
         item.price,
         item.categoryCodes,
         1,
-        options)
+        options
+    )
 
     var count = count
-    private set(value) {
-        field = value
-    }
+        private set(value) {
+            field = value
+        }
 
-    val finalPrice : Int
-    get() = (optionItems.map { it.finalPrice }.sum() + price) * count
+    val finalPrice: Int
+        @Exclude
+        get() = (optionItems.map { it.finalPrice }.sum() + price) * count
 
     override fun equals(other: Any?): Boolean {
         return other is OrderItem &&
@@ -32,12 +36,12 @@ open class OrderItem(
                 other.optionItems == optionItems
     }
 
-    fun plus(item: OrderItem){
+    fun plus(item: OrderItem) {
         require(code == item.code)
         plusCart(item.count)
     }
 
-    fun plusCart(delta:Int){
+    fun plusCart(delta: Int) {
         count += delta
     }
 }
--- app/src/main/java/kr/evalon/orderservice/scene/order/cart/CartOrderItemVm.kt
+++ app/src/main/java/kr/evalon/orderservice/scene/order/cart/CartOrderItemVm.kt
@@ -7,19 +7,24 @@ import kr.evalon.orderservice.models.OrderItem
 import kr.evalon.orderservice.toFormattedPrice
 
 class CartOrderItemVm(val model:OrderItem, val imgUrl:String) {
-    private val code = model.code
     val name = model.name
     val countLiveData = MutableLiveData<Int>().apply { value = model.count }
     val countText: LiveData<String> = Transformations.map(countLiveData) {
         it?.toString() ?: model.count.toString()
     }
     val priceText : String = model.finalPrice.toFormattedPrice()
+    val optionText = model.optionItems.take(MAX_OPTION_VISIBLE).mapIndexed { index, orderItem ->
+        val isLastTake = index == MAX_OPTION_VISIBLE - 1
+        val remainOtherOption = index != model.optionItems.lastIndex
+        if(isLastTake && remainOtherOption) "..."
+        else "+ ${orderItem.name}"
+    }.joinToString("\n")
 
     fun has(vm:CartOrderItemVm) = model.code == vm.model.code
 
     override fun equals(other: Any?): Boolean {
         if(other !is CartOrderItemVm) return false
-        return other.code == code && other.countLiveData.value == this.countLiveData.value
+        return other.model == model
     }
 
     override fun hashCode(): Int {
@@ -30,5 +35,9 @@ class CartOrderItemVm(val model:OrderItem, val imgUrl:String) {
         if(model.count + delta < 0) return
         model.plusCart(delta)
         countLiveData.value = model.count
+    }
+
+    companion object {
+        private const val MAX_OPTION_VISIBLE = 3
     }
 }
